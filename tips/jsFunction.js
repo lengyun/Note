bf0246c5c32881b3f4776897323490ad49d05b11
chris.li <lengyun@users.noreply.github.com>
2016-03-23 11:24:07 +0800

    Update jsFunction.js
    function change

--- tips/jsFunction.js
+++ tips/jsFunction.js
@@ -1,13 +1,10 @@
-encodeURIComponent()
-decodeURIComponent()
-
-encodeURI()
-decodeURI()
-
-escape()
-unescape()
-
-
+encodeURIComponent()编码URL组件
+decodeURIComponent()解码URL组件
+encodeURI()字符串作为 URI 进行编码
+decodeURI()解码某个编码的 URI
+如果 URI 组件中含有分隔符，比如 ? 和 #，则应当使用 encodeURIComponent() 方法分别对各组件进行编码。
+escape()对字符串进行编码，这样就可以在所有的计算机上读取该字符串。
+unescape()对由 escape() 编码的字符串进行解码
 
 parseFloat() 函数可解析一个字符串，并返回一个浮点数。
 toFixed(2) 方法可把 Number 四舍五入为指定小数位数的数字。
@@ -19,14 +16,5 @@ Screen 对象包含有关客户端显示屏幕的信息。（http://www.w3school
 getTimezoneOffset() 方法可返回格林威治时间和本地时间之间的时差，以分钟为单位
 getTime() 方法可返回距 1970 年 1 月 1 日之间的毫秒数。
 random() 方法可返回介于 0 ~ 1 之间的一个随机数。
-escape() 函数可对字符串进行编码，这样就可以在所有的计算机上读取该字符串。
 parseInt() 函数可解析一个字符串，并返回一个整数
 javaEnabled() 方法可返回一个布尔值，该值指示浏览器是否支持并启用了 Java。如果是，则返回 true，否则返回 false。
-
-
-http://i5ting.github.io/node-debug-tutorial/#102
-http://www.2cto.com/kf/201312/265979.html
-Riot.js：不足1KB的MVP客户端框架
-http://riotjs.com/
-reactjs
-$routeParams
